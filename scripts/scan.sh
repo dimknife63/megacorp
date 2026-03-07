0852457657c56e4e8b115aaa4321226d0d5c749d
dimknife63 <email@example.com>
2026-03-07 13:13:10 -0500

    K: add scanner

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,16 @@
-# TODO: write the script
+printf "
+====== SCANNING FOR CREDIT CARD NUMBERS ======
+"
+grep -rE --color=always '([0-9]{4}[- ]?){3}[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= CREDIT CARD SCAN COMPLETE =========="
+printf "
+==== SCANNING FOR SOCIAL SECURITY NUMBERS ====
+"
+grep -rE --color=always '[0-9]{3}-[0-9]{2}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
+printf "
+========= SCANNING FOR PHONE NUMBERS =========
+"
+grep -rE --color=always '[0-9]{3}-[0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= PHONE NUMBER SCAN COMPLETE ========="
